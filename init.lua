793a9193077d45b7dfcc7425485f654c80d4480c
David Levine <levine1726@protonmail.com>
2023-01-09 10:23:06 -0500

    switch to carbonfox

--- init.lua
+++ init.lua
@@ -26,7 +26,7 @@ local config = {
         },
 
         -- Set colorscheme to use
-        colorscheme = "nightfox",
+        colorscheme = "carbonfox",
 
         -- Add highlight groups in any theme
         highlights = {
