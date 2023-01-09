5c9b259c9536df9768df506045b9998661e063ca
David Levine <levine1726@protonmail.com>
2023-01-09 11:00:02 -0500

    configure custom elixir vscode snippets

--- init.lua
+++ init.lua
@@ -314,7 +314,9 @@ local config = {
                 -- Configure luasnip loaders (vscode, lua, and/or snipmate)
                 vscode = {
                         -- Add paths for including more VS Code style snippets in luasnip
-                        paths = {},
+                        paths = {
+                                "./lua/user/snippets",
+                        },
                 },
         },
 
