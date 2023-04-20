2e8f6aeb17b764a76ed89930430ad39b13cd61b8
David Levine <levine1726@protonmail.com>
2023-04-20 13:55:06 -0400

    configure null-ls to use prettierrc.json

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -2,7 +2,8 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
+    local formatting = null_ls.builtins.formatting
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -10,7 +11,11 @@ return {
     config.sources = {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      formatting.prettier.with {
+        env = {
+          PRETTIERD_DEFAULT_CONFIG = vim.fn.stdpath "config" .. "/.prettierrc.json",
+        },
+      }
     }
     return config -- return final config table
   end,
