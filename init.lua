e09a3b8e4dc083c1e208ee3bb4a8203abfbec7e4
David Levine <levine1726@protonmail.com>
2024-02-12 14:21:56 -0500

    Revert "setup transparent colorscheme"
    This reverts commit 47a691b05d52b5c2a1b593bd8f161162f458b143.

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "kanagawa-lotus",
+  colorscheme = "moonfly",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/community.lua
+++ plugins/community.lua
@@ -4,12 +4,8 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  
-  { import = "astrocommunity.color.transparent-nvim"},
-  { import = "astrocommunity.colorscheme.kanagawa-nvim"},
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
-  
   {
     -- further customize the options set by the community
     "copilot.lua",
